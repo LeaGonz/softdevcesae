39f552f837f8b46fa64b5987473ef48eca1759c7
LeaGonz <leag1001@hotmail.com>
2024-11-26 09:13:06 +0000

    Java casa

--- Algoritmos_Programação/Algoritmos/PL03/PL03_1/src/Main.java
+++ Algoritmos_Programação/Algoritmos/PL03/PL03_1/src/Main.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     // Variable global, se debe colocar el static
     static Scanner in = new Scanner(System.in);
@@ -50,32 +48,42 @@ public class Main {
         System.out.println("A substração é: " + subtracao);
     }
     private static void ListaNumeros() {
-        System.out.println("Aplicação 2: Lista de números");
-        System.out.println("Insira o inicio da lista:");
-        int numero1 = in.nextInt();
-        System.out.println("Insira o final da lista:");
-        int numero2 = in.nextInt();
-        String lista = "";
-        if (numero1 < numero2) {
-            for (int i = numero1; i < numero2; i++) {
-                lista += i + ",";
+        int op = 1;
+        do{
+            System.out.println("Aplicação 2: Lista de números");
+            System.out.println("Insira o inicio da lista:");
+            int numero1 = in.nextInt();
+            System.out.println("Insira o final da lista:");
+            int numero2 = in.nextInt();
+            String lista = "";
+            if (numero1 < numero2) {
+                for (int i = numero1; i < numero2; i++) {
+                    lista += i + ",";
+                }
+            } else {
+                for (int i = numero1; i > numero2; i--) {
+                    lista += i + ",";
+                }
             }
-        }
-        if (numero1 > numero2) {
-            for (int i = numero1; i > numero2; i--) {
-                lista += i + ",";
-            }
-        }
             System.out.println("Lista: " + lista + numero2);
+            System.out.println("Deseja criar uma nova lista?");
+            System.out.println("1- Sim");
+            System.out.println("0- Não");
+            op = in.nextInt();
+            if (op != 1 && op != 0) {
+                System.out.println("Opção invalida. Insira a correta:");
+                op = in.nextInt();
+            }
+        } while (op != 0);
     }
         public static void main (String[]args){
             int op;
-
             do {
                 System.out.println("Bem-vindo");
                 System.out.println("Escolha uma das seguintes opções:");
                 System.out.println("1- Calculadora");
                 System.out.println("2- Lista de números");
+                System.out.println("4- Jogo: Adivinhe o Número");
                 System.out.println("0 - Sair");
                 op = in.nextInt();
 
@@ -85,7 +93,10 @@ public class Main {
                     Calculadora();
                 } else if (op == 2) {
                     ListaNumeros();
-                } else {
+                } else if (op == 4) {
+                    AdivinheNumero();
+                } 
+                else {
                     System.out.println("Opção invalida");
                 }
             } while (op != 0);
