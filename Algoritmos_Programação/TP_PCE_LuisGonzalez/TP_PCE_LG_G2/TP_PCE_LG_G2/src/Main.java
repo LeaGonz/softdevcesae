83facc6bfe3066154ffa3d188c80b60de8c40d7c
LeaGonz <leag1001@hotmail.com>
2024-12-21 21:59:32 +0000

    Java G2 terminado

--- Algoritmos_Programação/TP_PCE_LuisGonzalez/TP_PCE_LG_G2/TP_PCE_LG_G2/src/Main.java
+++ Algoritmos_Programação/TP_PCE_LuisGonzalez/TP_PCE_LG_G2/TP_PCE_LG_G2/src/Main.java
@@ -1,38 +1,25 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 
 public class Main {
+    static Scanner in = new Scanner(System.in);
+    static Random rnd = new Random();
+    // Colores
     static final String reset = "\033[0m";
     static final String vermelho = "\033[0;31m";
     static final String verde = "\033[0;32m";
     static final String amarelo = "\033[0;33m";
-
-    static Scanner in = new Scanner(System.in);
-    static Random rnd = new Random();
-
+    // Variaveis principais
     static ArrayList<String> ouvintes = new ArrayList<>();
     static ArrayList<Integer> jogosTotal = new ArrayList<>();
     static ArrayList<Integer> jogosGanhos = new ArrayList<>();
+    // Variaveis só para ranking
+    static ArrayList<String> ouvintest;
+    static ArrayList<Integer> jogosTotalt, jogosGanhost;
 
     public static void main(String[] args) {
-        /*
-A “Anabela de Malhadas” margem de 150g (ex.: entre4,200kg e 4,350kg)
-Faça uma aplicação que permita aos ouvintes jogar no “Jogo do Saco”. Implemente as seguintes opções:
-1. Adicionar e remover ouvintes.
-2. Ver ranking de ouvintes (número de vezes que jogaram e número de vezes que acertaram).
-3. Jogar.
-4. Ler e gravar ranking em ficheiro.
-Em cada jogo devem ser selecionados de forma aleatória quais os ouvintes que vão jogar e a sua ordem. Em cada jogo o
-vencedor será o que se aproximar mais do valor do peso do saco.
-         */
-        ouvintes.add("luis");
-        ouvintes.add("andreina");
-        ouvintes.add("anabela");
-        jogosTotal.add(0);
-        jogosTotal.add(0);
-        jogosTotal.add(0);
-        jogosGanhos.add(0);
-        jogosGanhos.add(0);
-        jogosGanhos.add(0);
         int op;
         do {
             System.out.println(verde + "\nBEM-VINDO AO JOGO DO SACO" + reset);
@@ -58,8 +45,10 @@ vencedor será o que se aproximar mais do valor do peso do saco.
                     verRanking();
                     break;
                 case 4:
+                    gravarFicheiro();
                     break;
                 case 5:
+                    lerFicheiro();
                     break;
                 default:
                     System.out.println(amarelo + "Opção invalida!" + reset);
@@ -67,21 +56,113 @@ vencedor será o que se aproximar mais do valor do peso do saco.
         } while (op != 0);
     }
 
+    private static void limparDados() {
+        ouvintes.clear();
+        jogosTotal.clear();
+        jogosGanhos.clear();
+    } // validado
+
+    private static void lerFicheiro() {
+        System.out.println(verde + "\nLer Ficheiro do Raking do 'Jogo do Saco'" + reset);
+
+        try {
+            Path dirBD = Path.of("Ranking_BD.txt");
+            List<String> linhas = Files.readAllLines(dirBD);
+            if (!linhas.isEmpty()) {
+                limparDados();
+                for (String linha : linhas) {
+                    String[] lines = linha.split(";");
+                    ouvintes.add(lines[0]);
+                    jogosTotal.add(Integer.valueOf(lines[1]));
+                    jogosGanhos.add(Integer.valueOf(lines[2]));
+                }
+                System.out.println(verde + "Ficheiro lido com sucesso. Base de dados atualizada!" + reset);
+            } else {
+                System.out.println(amarelo + "Não existem dados no ficheiro." + reset);
+            }
+        } catch (IOException e) {
+            System.out.println(vermelho + "Não existe o ficheiro: " + e.getMessage() + reset);
+        }
+    } // validado
+
+    private static void gravarFicheiro() {
+        // Chamamos função ordenar
+        rankingOrder();
+
+        System.out.println(verde + "\nGravar em Ficheiro o Raking do 'Jogo do Saco'" + reset);
+        // Verificamos se existem ouvintes
+        if (!ouvintest.isEmpty()) {
+            // Path dos ficheiros: 1 do ranking, outro da base da dados dos ouvintes
+            Path dir = Path.of("Ranking.txt");
+            Path dirBD = Path.of("Ranking_BD.txt");
+
+            try {
+                // Preenchemos as linhas
+                String linesBD = "";
+                String lines = String.format("%-8s| %-22s| %-17s| %-8s%n", "Ranking", "Nomes", "Jogos Acertados", "Jogos");
+                lines += "-".repeat(60) + "\n";
+                for (int i = 0; i < ouvintest.size(); i++) {
+                    lines += String.format("%-8d| %-22s| %-17s| %-8s%n", i + 1, ouvintest.get(i), jogosGanhost.get(i), jogosTotalt.get(i));
+                    linesBD += ouvintest.get(i) + ";" + jogosTotalt.get(i) + ";" + jogosGanhost.get(i) + "\n";
+                }
+                // Escrevemos nos ficheiros
+                Files.writeString(dir, lines);
+                Files.writeString(dirBD, linesBD);
+                System.out.println(verde + "Ficheiro criado com sucesso!" + reset);
+            } catch (IOException e) {
+                System.out.println(vermelho + e.getMessage() + reset);
+            }
+
+        } else {
+            System.out.println(amarelo + "Não há informação para gravar no ficheiro." + reset);
+        }
+    } // validado
+
     private static void verRanking() {
+        // Chamamos função ordenar
+        rankingOrder();
+
         System.out.println(verde + "\nRaking do Jogo do Saco" + reset);
         // Listamos ouvintes se existem
-        if (!ouvintes.isEmpty()) {
-            System.out.printf(amarelo + "%-2s| %-22s| %-8s| %-8s%n" + reset, "ID", "Nomes", "Jogos", "Jogos Acertados");
-            System.out.println("-".repeat(50));
-            for (int i = 0; i < ouvintes.size(); i++) {
-                System.out.printf("%-2d| %-22s| %-8d| %-8d%n", i + 1, ouvintes.get(i), jogosTotal.get(i), jogosGanhos.get(i));
+        if (!ouvintest.isEmpty()) {
+            System.out.printf(amarelo + "%-8s| %-22s| %-17s| %-8s%n" + reset, "Ranking", "Nomes", "Jogos Acertados", "Jogos");
+            System.out.println("-".repeat(60));
+            for (int i = 0; i < ouvintest.size(); i++) {
+                System.out.printf("%-8d| %-22s| %-17s| %-8s%n", i + 1, ouvintest.get(i), jogosGanhost.get(i), jogosTotalt.get(i));
             }
-            return true;
         } else {
-            System.out.println(amarelo + "Não existem ouvintes para mostrar! Pode inserir na opção 1 do menu." + reset);
-            return false;
+            System.out.println(amarelo + "Não existem ouvintes para mostrar o raking." + reset);
         }
-    }
+    } // validado
+
+    private static void rankingOrder() {
+        // Preenchemos variaveis para ranking
+        ouvintest = new ArrayList<>(ouvintes);
+        jogosTotalt = new ArrayList<>(jogosTotal);
+        jogosGanhost = new ArrayList<>(jogosGanhos);
+
+        // Ordeno por jogos ganhos e em caso de empate por jogos totais
+        for (int i = 0; i < jogosGanhost.size(); i++) {
+            for (int j = i + 1; j < jogosGanhost.size(); j++) {
+                if ((jogosGanhost.get(i) < jogosGanhost.get(j)) || (jogosGanhost.get(i).equals(jogosGanhost.get(j)) && jogosTotalt.get(i) > jogosTotalt.get(j))) {
+                    // Ordenamos jogos ganhos
+                    int tempG = jogosGanhost.get(i);
+                    jogosGanhost.set(i, jogosGanhost.get(j));
+                    jogosGanhost.set(j, tempG);
+
+                    // Ordenamos jogos totales
+                    int tempT = jogosTotalt.get(i);
+                    jogosTotalt.set(i, jogosTotalt.get(j));
+                    jogosTotalt.set(j, tempT);
+
+                    // Ordenamos ouvintes
+                    String tempO = ouvintest.get(i);
+                    ouvintest.set(i, ouvintest.get(j));
+                    ouvintest.set(j, tempO);
+                }
+            }
+        }
+    } // validado
 
     private static void jogoSaco() {
         System.out.println(verde + "\nJOGO DO SACO" + reset);
@@ -111,7 +192,11 @@ vencedor será o que se aproximar mais do valor do peso do saco.
                     jogaram.add(jogador);
 
                     // Peso jogador aleatorio
-                    int pesoJogador = rnd.nextInt(pesoIni, (pesoIni + 151));
+                    int pesoJogador;
+                    if (ouvintes.get(jogador).equals("Anabela de Malhadas"))
+                        pesoJogador = rnd.nextInt(pesoIni - 999, pesoIni - 500);
+                    else pesoJogador = rnd.nextInt(pesoIni, (pesoIni + 151));
+
                     System.out.println("Jogador " + (i + 1) + ": " + ouvintes.get(jogador) + " diz que o peso correto é: " + verde + pesoJogador + "g." + reset);
 
                     // Preenchemos jogos totales
@@ -176,7 +261,7 @@ vencedor será o que se aproximar mais do valor do peso do saco.
                     removerOuvintes();
                     break;
                 default:
-                    System.out.println(amarelo + "Opção invalida!\n" + reset);
+                    System.out.println(amarelo + "Opção invalida!" + reset);
             }
         } while (op != 0);
     } // validado
@@ -202,6 +287,7 @@ vencedor será o que se aproximar mais do valor do peso do saco.
 
     private static void inserirOuvinte() {
         boolean inserido = false;
+        // Ciclo para inserir
         while (!inserido) {
             System.out.println(verde + "\nInserir Ouvinte" + reset);
             System.out.print("Nome: ");
@@ -215,6 +301,7 @@ vencedor será o que se aproximar mais do valor do peso do saco.
                 jogosTotal.add(0);
                 jogosGanhos.add(0);
                 System.out.println("Ouvinte " + verde + nome + reset + " inserido com sucesso!");
+                // Fechamos ciclo
                 inserido = true;
             }
         }
@@ -225,7 +312,7 @@ vencedor será o que se aproximar mais do valor do peso do saco.
         // Listamos ouvintes se existem
         if (!ouvintes.isEmpty()) {
             System.out.printf(amarelo + "%-2s| %-22s| %-8s| %-8s%n" + reset, "ID", "Nomes", "Jogos", "Jogos Acertados");
-            System.out.println("-".repeat(50));
+            System.out.println("-".repeat(55));
             for (int i = 0; i < ouvintes.size(); i++) {
                 System.out.printf("%-2d| %-22s| %-8d| %-8d%n", i + 1, ouvintes.get(i), jogosTotal.get(i), jogosGanhos.get(i));
             }
