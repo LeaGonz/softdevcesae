b4d07b7e572e79216a67931eaa370d6ac384e182
LeaGonz <leag1001@hotmail.com>
2025-01-28 10:24:27 +0000

    com php

---
+++ PHP (Laravel)/WebServerSide/resources/views/users/add_user.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.fo_layout')
+@section('content')
+    <h1>Olá, aqui podes Adicionar Utilizadores” e associe à rota.</h1>
+    <form action="">
+        <input type="text" name="name" id="name" placeholder="Nome">
+    </form>
+@endsection
