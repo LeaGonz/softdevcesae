dee7cd09f2e0fa5a78865716a4cd3c6bd791f238
LeaGonz <leag1001@hotmail.com>
2025-01-28 10:38:24 +0000

    laravel

---
+++ Laravel/WebServerSide/storage/framework/views/41f883f9a0dca9fc58826eda0c6b1cc2.php
@@ -0,0 +1,5 @@
+
+<?php $__env->startSection('content'); ?>
+    <h1><a href="<?php echo e(route('home')); ?>" >estás perdido volta aqui</a></h1>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.fo_layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\sw2024\Desktop\softdevcesae\PHP (Laravel)\WebServerSide\resources\views/fallback.blade.php ENDPATH**/ ?>
