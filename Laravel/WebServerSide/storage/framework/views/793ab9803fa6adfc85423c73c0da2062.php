a37ab37ed2537348bd3bda044a0959b4c3719751
LeaGonz <leag1001@hotmail.com>
2025-01-28 12:35:04 +0000

    laravel

--- Laravel/WebServerSide/storage/framework/views/793ab9803fa6adfc85423c73c0da2062.php
+++
@@ -1,8 +0,0 @@
-
-<?php $__env->startSection('content'); ?>
-    <h1>Olá, aqui podes Adicionar Utilizadores” e associe à rota.</h1>
-    <form action="">
-        <input type="text" name="name" id="name" placeholder="Nome">
-    </form>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.fo_layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\sw2024\Desktop\softdevcesae\PHP (Laravel)\WebServerSide\resources\views/users/add_user.blade.php ENDPATH**/ ?>
