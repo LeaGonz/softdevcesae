48fba08b01768d4d125e0df4d34b18ea738f3d71
LeaGonz <leag1001@hotmail.com>
2025-02-24 22:54:42 +0000

    change

--- Algoritmos_Programação/TP_POO_LuisGonzalez/JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"entidades"},{"l":"Enums"},{"l":"itens"},{"l":"jogo"}];updateSearchResults();
--- Algoritmos_Programação/TP_POO_LuisGonzalez/src/Enums/Personagem.java
+++
@@ -1,5 +0,0 @@
-package Enums;
-
-public enum Personagem {
-    Luffy, Zoro, Sanji, Usopp, Nami, Brook, Geral, Jack, Kurozumi, Who, Sasaki, Ulti, Queen, King, Kaido
-}
